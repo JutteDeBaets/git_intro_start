f53e055fe5f920787b38f06c76ff95cfa846da50
Jutte De Baets <Jutte.debaets@hotmail.com>
2026-03-13 17:54:22 +0100

    test inputs ino

--- arduino/test_outputs/test_outputs.ino
+++ arduino/test_outputs/test_outputs.ino
@@ -1,12 +1,14 @@
 int ledPin = 3;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);  
-  delay(1000);                      
+  delay(1000);        
+  Serial.println(“hello”);              
   digitalWrite(ledPin, LOW);   
   delay(1000);                      
 }
